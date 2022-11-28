3ad545d2a616dcdfdfed1feb2bd7f2b36e4200cd
nico-bb <nicolas.barbas@protonmail.com>
2022-11-28 14:46:24 +0100

    added devise user

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
@@ -44,6 +45,7 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
+  
   # Raise exceptions for disallowed deprecations.
   config.active_support.disallowed_deprecation = :raise
 
