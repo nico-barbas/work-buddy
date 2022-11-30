369b4654b7406b03e17d79732d0a9c386ca4dfde
Cannelle Magdane <cannelle.magdane@gmail.com>
2022-11-30 10:35:56 +0100

    continue back end of timer panel

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,8 @@ class UsersController < ApplicationController
   def show
     @task = Task.new
     @tasks = Task.all
-    @timer = Timer.new
     @timers = Timer.all
+    # how to find the timer when it is already existing? 
   end
 
   def game
