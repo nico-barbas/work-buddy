5241ef23c95ce62da8d90700ef590c54df3985ff
marine irigoin <marine.irigoin@gmail.com>
2022-12-06 15:06:29 +0100

    changed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ Avatar.destroy_all
 User.destroy_all
 
 
-new_user = User.new(email: "user@example.com", password: "password", username: "Nicolas")
+new_user = User.new(email: "nicolas@email.com", password: "password", username: "Nicolas")
 new_user.save!
 p "user created (with id #{new_user.id})"
 
@@ -21,17 +21,23 @@ new_avatar = Avatar.new(name: "Sherb", user: new_user)
 new_avatar.save!
 p "avatar created"
 
-label_test = Label.new(name: "General tasks", user: new_user)
+label_test = Label.new(name: "Meeting & call", user: new_user)
 label_test.save!
+label_test2 = Label.new(name: "Coding", user: new_user)
+label_test2.save!
+label_test3 = Label.new(name: "Paperwork", user: new_user)
+label_test3.save!
+label_test4 = Label.new(name: "Test", user: new_user)
+label_test4.save!
 p "label 'General tasks' created"
 
 new_timer = Timer.new(started_at: Time.now, label: label_test, user: new_user)
 new_timer.save!
 
-new_task = Task.new(title: "Review Cannelle's stimulus", description: "Stimulus is a nightmare so need double review", user: new_user)
+new_task = Task.new(title: "Implemente the chat feature", description: "Find ressources on how to do this", user: new_user)
 new_task.save!
-new_task2 = Task.new(title: "Add avatar personalisation", description: "We should be able to choose between different persona", user: new_user)
+new_task2 = Task.new(title: "Prepare bugs review", description: "For Monday meeting", user: new_user)
 new_task2.save!
-new_task3 = Task.new(title: "Help Marine with front end", description: "Front end is the devil", user: new_user)
+new_task3 = Task.new(title: "URSAFF declaration", description: "The worst part about being a freelance ", user: new_user)
 new_task3.save!
 p "tasks created"
