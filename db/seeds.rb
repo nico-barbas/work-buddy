80817591011e644e14fd11825b33a5e09d07f839
marine irigoin <marine.irigoin@gmail.com>
2022-12-08 16:17:43 +0100

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,15 +24,15 @@ p "avatar created"
 label_test = Label.new(name: "General tasks", user: new_user)
 label_test.total_label_time = 20160000
 label_test.save!
+
 label_test2 = Label.new(name: "Coding", user: new_user)
 label_test2.total_label_time = 203580000
 label_test2.save!
+
 label_test5 = Label.new(name: "Meeting & call", user: new_user)
 label_test5.total_label_time = 64816000
 label_test5.save!
-label_test3 = Label.new(name: "Paperwork", user: new_user)
-label_test3.total_label_time = 46412000
-label_test3.save!
+
 label_test4 = Label.new(name: "Testing", user: new_user)
 label_test4.total_label_time = 19628000
 label_test4.save!
@@ -42,18 +42,17 @@ new_timer = Timer.new(started_at: Time.now, label: label_test, user: new_user, t
 label_test.daily_label_time = new_timer.total_time
 new_timer.save!
 label_test.save
+
 new_timer = Timer.new(started_at: Time.now, label: label_test2, user: new_user, total_time: 16416000, logged: true, logged_date: Date.today)
 label_test2.daily_label_time = new_timer.total_time
 new_timer.save!
 label_test2.save
+
 new_timer = Timer.new(started_at: Time.now, label: label_test5, user: new_user, total_time: 6408000, logged: true, logged_date: Date.today)
 label_test5.daily_label_time = new_timer.total_time
 new_timer.save!
 label_test5.save
-new_timer = Timer.new(started_at: Time.now, label: label_test3, user: new_user, total_time: 3960000, logged: true, logged_date: Date.today)
-label_test3.daily_label_time = new_timer.total_time
-new_timer.save!
-label_test3.save
+
 new_timer = Timer.new(started_at: Time.now, label: label_test4, user: new_user, total_time: 3240000, logged: true, logged_date: Date.today)
 label_test4.daily_label_time = new_timer.total_time
 new_timer.save!
@@ -61,8 +60,11 @@ label_test4.save
 
 new_task = Task.new(title: "Implement the chat feature", user: new_user)
 new_task.save!
+
 new_task2 = Task.new(title: "Prepare bugs review", description: "For Monday meeting", user: new_user)
 new_task2.save!
+
 new_task3 = Task.new(title: "Review Cannelle's code", user: new_user)
 new_task3.save!
+
 p "tasks created"
